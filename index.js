f4685625209132004f9102f4b95415772f386797
vaibhav110800 <vibhu1108@gmail.com>
2020-07-24 01:43:53 +0530

    commit 10
    added team section

--- index.js
+++ index.js
@@ -73,14 +73,14 @@
 //   });
 // });
 
-$(document).ready(function () {
-    $("#testimonial-slider").owlCarousel({
-        items: 3,
-        itemsDesktop: [1000, 3],
-        itemsDesktopSmall: [979, 2],
-        itemsTablet: [768, 2],
-        itemsMobile: [650, 1],
-        pagination: true,
-        autoPlay: true
-    });
-});
+// $(document).ready(function () {
+//     $("#testimonial-slider").owlCarousel({
+//         items: 3,
+//         itemsDesktop: [1000, 3],
+//         itemsDesktopSmall: [979, 2],
+//         itemsTablet: [768, 2],
+//         itemsMobile: [650, 1],
+//         pagination: true,
+//         autoPlay: true
+//     });
+// });
