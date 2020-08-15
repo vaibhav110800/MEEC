9922aa9732714eb19a29b1ef7a06d20cb5db7a21
vaibhav110800 <vibhu1108@gmail.com>
2020-08-15 18:09:07 +0530

    commit 30
    added courses section

---
+++ component/courses/courses.js
@@ -0,0 +1,89 @@
+// window.alert("sometext");
+
+document.getElementById('1').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: block";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('2').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: block";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('3').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: block";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('4').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: block";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('5').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: block";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('6').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: block";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('7').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: block";
+    document.getElementById('courses8').style = "display: none";
+}
+
+document.getElementById('8').onclick = function changeContent() {
+    document.getElementById('courses1').style = "display: none";
+    document.getElementById('courses2').style = "display: none";
+    document.getElementById('courses3').style = "display: none";
+    document.getElementById('courses4').style = "display: none";
+    document.getElementById('courses5').style = "display: none";
+    document.getElementById('courses6').style = "display: none";
+    document.getElementById('courses7').style = "display: none";
+    document.getElementById('courses8').style = "display: block";
+}
